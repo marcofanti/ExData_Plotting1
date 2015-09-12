4cd6dbc7e3beb6486091546f76f4b13c2a6abb0b
Marco Fanti <marco.fanti@gmail.com>
2015-09-12 06:56:04 -0400

    Fixed plot4.R

--- plot4.R
+++ plot4.R
@@ -1,19 +1,31 @@
 # Read the data
-data <- read.csv("~/Development/R_dir/Exploratory_Data_Analysis/ExData_Plotting1/data/household_power_consumption.txt", na.string="?", sep=";")
+data <- read.csv("~/Development/R_dir/Exploratory_Data_Analysis/ExData_Plotting1/data/household_power_consumption.txt", 
+                 na.string="?", sep=";")
 
 # Extract needed dataset
 data <- data[(data$Date=="1/2/2007" | data$Date=="2/2/2007"),]
 
 # Combine Date and Time
-data$DateTime <- strptime(paste(data$Date, data$Time, sep=" "), 
-                          format="%d/%m/%Y %H:%M:%S")
+data$DateTime <- strptime(paste(data$Date, data$Time, sep=" "), format="%d/%m/%Y %H:%M:%S")
 
 # Open png device
-png("~/Development/R_dir/Exploratory_Data_Analysis/ExData_Plotting1/plot1.png", width=480, height=480)
+png("~/Development/R_dir/Exploratory_Data_Analysis/ExData_Plotting1/plot4.png", width=480, height=480)
 
-# Plot plot1
-hist(data$Global_active_power, main="Global Active Power", 
-     xlab="Global Active Power (kilowatts)", col="red")
+# Plot plot4
+par(mfrow=c(2, 2))
+
+plot(data$DateTime, data$Global_active_power, type="l", 
+     xlab="", ylab="Global Active Power")
+plot(data$DateTime, data$Voltage, type="l", 
+     xlab="datetime", ylab="Voltage")
+plot(data$DateTime, data$Sub_metering_1, type="l", 
+     xlab="", ylab="Energy sub metering")
+lines(data$DateTime, data$Sub_metering_2, col="red")
+lines(data$DateTime, data$Sub_metering_3, col="blue")
+legend("topright", bty="n", lty=1, col=c("black", "red", "blue"), 
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+plot(data$DateTime, data$Global_reactive_power, type="l", 
+     xlab="datetime", ylab="Global_reactive_Power")
 
 # Turn off device
 dev.off()
